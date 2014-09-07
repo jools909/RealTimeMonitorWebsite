6b365653dc189fd109b6dfbdce3cdd8cc26a694d
Jools <jools909@hotmail.co.uk>
2014-09-07 16:40:25 +0100

    Added php layout

---
+++ footer.php
@@ -0,0 +1,30 @@
+        <footer>
+            <div class="footer-content">
+
+                <!--
+                    footer content goes here
+
+                -->
+
+
+            </div><!-- end footer content -->
+        </footer><!-- end footer wrap -->
+
+
+        <!-- jqueryfuncitons -->
+        <script type="text/javascript" src="js/jquery-functions.js"></script>
+        <!-- Modernizer - http://modernizr.com/download/#-shiv-cssclasses-load -->
+        <script type="text/javascript" src="js/modernizr.custom.js"></script>
+        <!-- Classie - class helper functions by @desandro https://github.com/desandro/classie -->
+        <script type="text/javascript" src="js/classie.js"></script>
+        <!-- slider panel funcitons -->
+        <script type="text/javascript" src="js/panel-functions.js"></script>
+        <!-- settings toggle functions -->
+        <script type="text/javascript" src="js/setting-switches.js"></script>
+        <!-- search javascript -->
+        <script type="text/javascript" src="js/search.js"></script>
+        <!-- chat javascript -->
+        <script type="text/javascript" src="js/chat.js"></script>
+
+    </body>
+</html>
---
+++ layout.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+    <head>
+
+         <!-- meta -->
+        <meta charset="UTF-8">
+        <meta name="description" content=" " />
+        <meta name="keywords" content=" " />
+        <meta name="author" content=" " />
+
+        <link rel="stylesheet" type="text/css" media="all" href="css/main.css" />
+
+        <title>Real-Time Sensor Monitoring</title>
+    </head>
