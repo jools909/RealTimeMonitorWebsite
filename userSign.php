588b78277c34dd7827ee7bf619fcc89a562d04cb
Jools <jools909@hotmail.co.uk>
2014-09-10 19:47:32 +0100

    Sign in page now forwarding to data page.

--- userSign.php
+++ userSign.php
@@ -1,17 +1,27 @@
 <?php
 $signInDetails = array();
 //Get username from post
-if(isset($post['username'])){
-    $signInDetails['username'] = trim(htmlspecialchars($post['username']));
+if(isset($_POST['username'])){
+    $signInDetails['username'] = trim(htmlspecialchars($_POST['username']));
 }
 
 //Get password from post
-if(isset($post['password']) && trim(htmlspecialchars($post['password'])) != ""){
-    $signInDetails['password'] = crypt(htmlspecialchars($post['password']), '$2a$07$7A0w1YreTefVmuUcjT0RgdQn4$');
+if(isset($_POST['password']) && trim(htmlspecialchars($_POST['password'])) != ""){
+    $signInDetails['password'] = crypt(htmlspecialchars($_POST['password']), '$2a$07$7A0w1YreTefVmuUcjT0RgdQn4$');
 }
 
-if($signInDetails['username'] == 'azureuser' && $signInDetails['password'] == '$2a$07$7A0w1YreTefVmuUcjT0RgOn9PXIjjF90Qd1hTMj/DYK1jppaGCydO'){
-    $redirecturl = "Location: http://" . htmlspecialchars($_SERVER['HTTP_HOST']) . dirname(htmlspecialchars($_SERVER['PHP_SELF'])) . "/data.html";
-    header($redirecturl);
+if(isset($signInDetails['username']) && isset($signInDetails['password'])){
+    if($signInDetails['username'] == 'azureuser' && $signInDetails['password'] == '$2a$07$7A0w1YreTefVmuUcjT0RgOn9PXIjjF90Qd1hTMj/DYK1jppaGCydO'){
+        $redirecturl = "Location: http://" . htmlspecialchars($_SERVER['HTTP_HOST']) . dirname(htmlspecialchars($_SERVER['PHP_SELF'])) . "/data.html";
+        header($redirecturl);
+        exit;
+    } else {
+        $homeURL = "Location: http://". htmlspecialchars($_SERVER['HTTP_HOST']) . dirname(htmlspecialchars($_SERVER['PHP_SELF'])) . "/index.html";
+        header($homeURL);
+        exit;
+    }
+} else {
+    $homeURL = "Location: http://". htmlspecialchars($_SERVER['HTTP_HOST']) . dirname(htmlspecialchars($_SERVER['PHP_SELF'])) . "/index.html";
+    header($homeURL);
     exit;
 }
