32af0fe2b4d746001dd1b7729cc53b05a8c2a875
Jools <jools909@hotmail.co.uk>
2014-09-10 19:57:12 +0100

    Updated userSign.php to redirect properly with Azure 2

--- userSign.php
+++ userSign.php
@@ -12,13 +12,21 @@ if(isset($_POST['password']) && trim(htmlspecialchars($_POST['password'])) != ""
 
 if(isset($signInDetails['username']) && isset($signInDetails['password'])){
     if($signInDetails['username'] == 'azureuser' && $signInDetails['password'] == '$2a$07$7A0w1YreTefVmuUcjT0RgOn9PXIjjF90Qd1hTMj/DYK1jppaGCydO'){
+        echo htmlspecialchars($_SERVER['HTTP_HOST']);
+        echo dirname(htmlspecialchars($_SERVER['PHP_SELF']));
+
         $redirecturl = "Location: http://" . htmlspecialchars($_SERVER['HTTP_HOST']) . dirname(htmlspecialchars($_SERVER['PHP_SELF'])) . "data.html";
-        header($redirecturl);
-        exit;
+        echo $redirecturl;
+//        header($redirecturl);
+//        exit;
     } else {
+        echo htmlspecialchars($_SERVER['HTTP_HOST']);
+        echo dirname(htmlspecialchars($_SERVER['PHP_SELF']));
+
         $homeURL = "Location: http://". htmlspecialchars($_SERVER['HTTP_HOST']) . dirname(htmlspecialchars($_SERVER['PHP_SELF'])) . "index.html";
-        header($homeURL);
-        exit;
+        echo $homeURL;
+//        header($homeURL);
+//        exit;
     }
 } else {
     $homeURL = "Location: http://". htmlspecialchars($_SERVER['HTTP_HOST']) . dirname(htmlspecialchars($_SERVER['PHP_SELF'])) . "index.html";
