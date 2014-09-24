5038e3196ad30739d37020524015ca2ea38328f6
Jools <jools909@hotmail.co.uk>
2014-09-24 14:34:32 +0100

    updated layout.php

--- layout.php
+++ layout.php
@@ -8,7 +8,7 @@
         <meta name="keywords" content=" " />
         <meta name="author" content=" " />
 
-        <link rel="stylesheet" type="text/css" href="~/main.css">
+        <link rel="stylesheet" type="text/css" href="main.css" media="screen">
 
         <title>Real-Time Sensor Monitoring</title>
     </head>
