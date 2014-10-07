e781860ba9f36c22f518099aac504aa0ba9d82d3
cabox <cabox@box-codeanywhere.com>
2014-10-07 13:14:39 -0400

    testing 1 second refresh of graphs and table.

--- data.php
+++ data.php
@@ -1,6 +1,7 @@
 <?php include "layout.php"; ?>
 <?php include "header.php"; ?>
     <script type="text/javascript" src="Chart.js"></script>
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.0/jquery.min.js"></script>
 
 	
 		<div class="main">
@@ -19,85 +20,88 @@
                 </div>
 
                 <script type="text/javascript">
-                    if (window.XMLHttpRequest) {
-                        // code for IE7+, Firefox, Chrome, Opera, Safari
-                        xmlhttp=new XMLHttpRequest();
-                    } else { // code for IE6, IE5
-                        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-                    }
-                    xmlhttp.onreadystatechange=function() {
-                        if (xmlhttp.readyState==4 && xmlhttp.status==200) {
-                            var dataReturn = xmlhttp.responseText;
-
-                            var resultsArray = dataReturn.split(";");
-                            var final2DArray = [];
-
-                            for (var i = 0; i<resultsArray.length;i++){
-                                final2DArray[i] = resultsArray[i].split(",");
-                            } //10x11 2D Array now made
-
-                            var printHeader = "<table id='dataTable;'>" +
-                                "<tr><th>ReadingID</th><th>DateTime</th>" +
-                                "<th>Gyro x</th><th>Gyro y</th><th>Gyro z</th>" +
-                                "<th>Accel x</th><th>Accel y</th><th>Accel z</th>" +
-                                "<th>Compass x</th><th>Compass y</th><th>Compass z</th></tr>";
-                            var printData = "";
-                            for (var x = 0; x < final2DArray.length; x++){
-                                var printString = "<tr>";
-                                for (var y = 0; y < final2DArray[x].length; y++){
-                                    printString += "<td>" + final2DArray[x][y].toString() + "</td>";
-                                }
-                                printString += "</tr>";
-                                if (printString != "<tr><td></td></tr>"){
-                                    printData += printString;
-                                }
-                                printString = "";
-                            }
-                            printData += "</table>";
-                            //prompt("printString:",printHeader + printData);
-                            document.getElementById("results").innerHTML = printHeader + printData;
-
-                            var sensorData = [];
-                            for (var z = 0; z < 3; z++){
-                                sensorData[z] = {
-                                    labels : graphLabels(final2DArray),
-                                    datasets : [
-                                        {
-                                            fillColor : "rgba(172,194,132,0)",
-                                            strokeColor : "#ACC26D",
-                                            pointColor : "#fff",
-                                            pointStrokeColor : "#9DB86D",
-                                            data : graphData(final2DArray, ((3*z)+2))
-                                        },
-                                        {
-                                            fillColor : "rgba(172,194,132,0)",
-                                            strokeColor : "#75A3FF",
-                                            pointColor : "#fff",
-                                            pointStrokeColor : "#6699FF",
-                                            data : graphData(final2DArray, ((3*z)+2)+1)
-                                        },
-                                        {
-                                            fillColor : "rgba(172,194,132,0)",
-                                            strokeColor : "	#FFD119",
-                                            pointColor : "#fff",
-                                            pointStrokeColor : "#FFCC00",
-                                            data : graphData(final2DArray, ((3*z)+2)+2)
-                                        }
-                                    ]
-                                };
-                            }
-                            var ctx0 = document.getElementById("myChart0").getContext("2d");
-                            var myCharts0 = new Chart(ctx0).Line(sensorData[0]);
-
-                            var ctx1 = document.getElementById("myChart1").getContext("2d");
-                            var myCharts1 = new Chart(ctx1).Line(sensorData[1]);
-
-                            var ctx2 = document.getElementById("myChart2").getContext("2d");
-                            var myCharts2 = new Chart(ctx2).Line(sensorData[2]);
-                        }
-                    };
-                    xmlhttp.open("GET","dbConnect.php",true);
-                    xmlhttp.send();
+                    var auto_refresh = setInterval(
+					function (){
+						if (window.XMLHttpRequest) {
+							// code for IE7+, Firefox, Chrome, Opera, Safari
+							xmlhttp=new XMLHttpRequest();
+						} else { // code for IE6, IE5
+							xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+						}
+						xmlhttp.onreadystatechange=function() {
+							if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+								var dataReturn = xmlhttp.responseText;
+								
+								var resultsArray = dataReturn.split(";");
+								var final2DArray = [];
+								
+								for (var i = 0; i<resultsArray.length;i++){
+									final2DArray[i] = resultsArray[i].split(",");
+								} //10x11 2D Array now made
+								
+								var printHeader = "<table id='dataTable;'>" +
+									"<tr><th>ReadingID</th><th>DateTime</th>" +
+									"<th>Gyro x</th><th>Gyro y</th><th>Gyro z</th>" +
+									"<th>Accel x</th><th>Accel y</th><th>Accel z</th>" +
+									"<th>Compass x</th><th>Compass y</th><th>Compass z</th></tr>";
+								var printData = "";
+								for (var x = 0; x < final2DArray.length; x++){
+									var printString = "<tr>";
+									for (var y = 0; y < final2DArray[x].length; y++){
+										printString += "<td>" + final2DArray[x][y].toString() + "</td>";
+									}
+									printString += "</tr>";
+									if (printString != "<tr><td></td></tr>"){
+										printData += printString;
+									}
+									printString = "";
+								}
+								printData += "</table>";
+								//prompt("printString:",printHeader + printData);
+								document.getElementById("results").innerHTML = printHeader + printData;
+								
+								var sensorData = [];
+								for (var z = 0; z < 3; z++){
+									sensorData[z] = {
+										labels : graphLabels(final2DArray),
+										datasets : [
+											{
+												fillColor : "rgba(172,194,132,0)",
+												strokeColor : "#ACC26D",
+												pointColor : "#fff",
+												pointStrokeColor : "#9DB86D",
+												data : graphData(final2DArray, ((3*z)+2))
+											},
+											{
+												fillColor : "rgba(172,194,132,0)",
+												strokeColor : "#75A3FF",
+												pointColor : "#fff",
+												pointStrokeColor : "#6699FF",
+												data : graphData(final2DArray, ((3*z)+2)+1)
+											},
+											{
+												fillColor : "rgba(172,194,132,0)",
+												strokeColor : "	#FFD119",
+												pointColor : "#fff",
+												pointStrokeColor : "#FFCC00",
+												data : graphData(final2DArray, ((3*z)+2)+2)
+											}
+										]
+									};
+								}
+								var ctx0 = document.getElementById("myChart0").getContext("2d");
+								var myCharts0 = new Chart(ctx0).Line(sensorData[0]);
+								
+								var ctx1 = document.getElementById("myChart1").getContext("2d");
+								var myCharts1 = new Chart(ctx1).Line(sensorData[1]);
+								
+								var ctx2 = document.getElementById("myChart2").getContext("2d");
+								var myCharts2 = new Chart(ctx2).Line(sensorData[2]);
+							}
+						};
+						xmlhttp.open("GET","dbConnect.php",true);
+						xmlhttp.send();				
+					}, 1000);
 
                     function graphData(arrayInput, j){
                         var dataArray = [];
